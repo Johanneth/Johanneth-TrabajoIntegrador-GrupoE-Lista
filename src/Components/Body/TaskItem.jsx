b9003dbc37abc8261b39bbd56e2b9ac319c5f0a0
Johanneth <jaagav4@gmail.com>
2023-11-29 19:04:50 -0300

    modificacion de iconos

--- src/Components/Body/TaskItem.jsx
+++ src/Components/Body/TaskItem.jsx
@@ -1,6 +1,7 @@
 
 import React, { useState } from 'react';
 import './TaskItem.css';
+import { BsCheck2Square, BsSquare } from "react-icons/bs";
 
 const TaskItem = ({ task, onTaskComplete, onDeleteTask }) => {
   const [completed, setCompleted] = useState(false);
@@ -11,11 +12,15 @@ const TaskItem = ({ task, onTaskComplete, onDeleteTask }) => {
   };
 
   return (
-    <div className={`task-row ${completed ? 'completed' : ''}`}>
+    <div className={`task-row ${completed ? 'completed' : ''}`} style={{ color: completed && "#000000" , textDecoration: completed ? "line-through" : "none" }}>
       <div className="left-column">
-        <div className="event-square" onClick={handleComplete}></div>
+      <div onClick={handleComplete}>
+        {completed ? <BsCheck2Square size={20
+        } /> : <BsSquare size={16} />}
       </div>
-      <div className="right-column">{task.description}</div>
+        {/* <div className="event-square" onClick={handleComplete}></div> */}
+      </div>
+      <div className="right-column">{task.description} </div>
       <div className="delete-button" onClick={() => onDeleteTask(task.id)}>
         X
       </div>
